cc8fdafa94441f982235973fcd86c31864169735
Shen Nan <wongshennan@live.com>
2021-12-05 21:08:00 +0800

    added options for production deploy

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ const Home = () => {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      Test Deployment
     </div>
   )
 }
