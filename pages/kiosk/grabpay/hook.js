93733076c47eb76dfea2cb4db627f58474a90fa0
Shen Nan, Wong <wongshennan@gmail.com>
2026-04-30 09:43:49 +0700

    checkpoint: edit /Users/wongshennan/Documents/personal/learning/bootcamps/rocketacademy/restaurant/pages/kiosk/grabpay/hook.js

--- pages/kiosk/grabpay/hook.js
+++ pages/kiosk/grabpay/hook.js
@@ -6,7 +6,8 @@ const RedirectHook = () => {
     const paymentIntent = new URLSearchParams(window.location.search).get('payment_intent')
     const clientSecret = new URLSearchParams(window.location.search).get("payment_intent_client_secret")
     const redirectStatus = new URLSearchParams(window.location.search).get("redirect_status")
-    const redirectURL = `http://localhost:3000/kiosk/payment_success?paymentIntent=${paymentIntent}&paymentMethod=grabPay`
+    const baseUrl = process.env.APP_DOMAIN || 'http://localhost:3000'
+    const redirectURL = `${baseUrl}/kiosk/payment_success?paymentIntent=${paymentIntent}&paymentMethod=grabPay`
 
     if(redirectStatus == "succeeded") {
       window.opener.location.replace(redirectURL)
