4850d7ba7d8dd00a952b1dc8c3f400eb519a554d
Shen Nan, Wong <wongshennan@gmail.com>
2026-04-30 09:43:52 +0700

    checkpoint: edit /Users/wongshennan/Documents/personal/learning/bootcamps/rocketacademy/restaurant/pages/api/terminal/register_reader.js

--- pages/api/terminal/register_reader.js
+++ pages/api/terminal/register_reader.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const RegisterReader = async (req,res) => {
 
-  const { registration_code, label, location } = req.params
+  const { registration_code, label, location } = req.body
 
     const reader = await stripe.terminal.reader.create(registration_code, label, location)
     res.send(reader)
