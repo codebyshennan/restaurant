df622aa8708b2f387f5e5234330d9beffba984ee
Shen Nan, Wong <wongshennan@gmail.com>
2026-04-30 09:44:19 +0700

    checkpoint: edit /Users/wongshennan/Documents/personal/learning/bootcamps/rocketacademy/restaurant/pages/api/kitchen/orders.js

--- pages/api/kitchen/orders.js
+++ pages/api/kitchen/orders.js
@@ -3,36 +3,43 @@ const ObjectID = require('mongodb').ObjectID
 
 const ordersHandler = async (req, res) => {
 
-  //get the orders and paginate accordingly
-  const client = await dbConnection()
-  const db = client.db('fastfood')
   const { method } = req
 
-  if(method == "GET") {
-    const ordersData = await db.collection('orders').find({}).toArray()
-    res.status(200).send(ordersData)
+  if(method === "GET") {
+    try {
+      //get the orders and paginate accordingly
+      const client = await dbConnection()
+      const db = client.db('fastfood')
+      const ordersData = await db.collection('orders').find({}).toArray()
+      res.status(200).send(ordersData)
+    } catch (error) {
+      res.status(500).send({ message: new Error(error).message, success: false })
+    }
   }
 
-  if(method == "PUT") {
-    
-    // delete
-    const id = req.body
-    console.log(id)
-    const updateOrder = await db.collection('orders').updateOne({"_id": ObjectID(id)}, {$set: {"status": "ready"}}, {upsert: true})
-    console.log(updateOrder)
-    res.send(updateOrder)
+  if(method === "PUT") {
+    try {
+      const client = await dbConnection()
+      const db = client.db('fastfood')
+      const id = req.body
+      const updateOrder = await db.collection('orders').updateOne({"_id": ObjectID(id)}, {$set: {"status": "ready"}}, {upsert: false})
+      res.send(updateOrder)
+    } catch (error) {
+      res.status(500).send({ message: new Error(error).message, success: false })
+    }
   }
 
-  if(method == "DELETE") {
-    
-    // delete
-    const id = req.body
-    console.log(id)
-    const updateOrder = await db.collection('orders').updateOne({"_id": ObjectID(id)}, {$set: {"status": "completed", "completed_at": new Date()}}, {upsert: true})
-    console.log(updateOrder)
-    res.send(updateOrder)
+  if(method === "DELETE") {
+    try {
+      const client = await dbConnection()
+      const db = client.db('fastfood')
+      const id = req.body
+      const updateOrder = await db.collection('orders').updateOne({"_id": ObjectID(id)}, {$set: {"status": "completed", "completed_at": new Date()}}, {upsert: false})
+      res.send(updateOrder)
+    } catch (error) {
+      res.status(500).send({ message: new Error(error).message, success: false })
+    }
   }
-
 
 }
 
